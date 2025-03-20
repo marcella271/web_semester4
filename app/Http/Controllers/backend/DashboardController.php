65c89b7e3f3ef732c7a38dc6e02825d4268aead3
marcella271 <158534226+marcella271@users.noreply.github.com>
2025-03-20 21:51:07 +0700

    Update DashboardController.php

--- app/Http/Controllers/backend/DashboardController.php
+++ app/Http/Controllers/backend/DashboardController.php
@@ -1,15 +1,1 @@
-<?php
-
-
-namespace App\Http\Controllers\backend;
-
-use Illuminate\Http\Request;
-use App\Http\Controllers\Controller;
-
-class DashboardController extends Controller
-{
-    public function index()
-    {
-        return view('backend.dashboard');
-    }
-}
+..
