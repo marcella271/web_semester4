a152d1fd40a022965d954990c8dd18631754b89b
marcella271 <158534226+marcella271@users.noreply.github.com>
2025-03-21 00:19:38 +0700

    Acara 11

--- vite.config.js
+++ vite.config.js
@@ -1,1 +1,11 @@
-d
+import { defineConfig } from 'vite';
+import laravel from 'laravel-vite-plugin';
+
+export default defineConfig({
+    plugins: [
+        laravel({
+            input: ['resources/css/app.css', 'resources/js/app.js'],
+            refresh: true,
+        }),
+    ],
+});
